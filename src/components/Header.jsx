4837675a3587361167d8672b0bde4394514d2d51
Hugo1208-6 <hmbumba@eagles.usi.edu>
2022-06-12 22:56:44 -0700

    thirdcommit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from './img/logo.png'
 import Avatar from './img/avatar.png'
 import {FaCartPlus } from "react-icons/fa";
+import { MdAdd,  MdLogout } from "react-icons/md";
 import {motion} from "framer-motion"
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
 import { Link } from 'react-router-dom';
+import { useStateValue } from '../context/StateProvider';
+import { actionType } from '../context/reducer';
 const Header = () => {
 
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
-  const login = async()=> {
-    const response = await signInWithPopup(firebaseAuth, provider)
+  const[{user}, dispatch] = useStateValue();
 
-    console.log(response)
+  const [isMenu, setIsMenu] = useState(false);
+
+  const login = async()=> {
+    if(!user) {
+    const {user : {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider)
+    dispatch({
+      type: actionType.SET_USER,
+      user : providerData[0],
+    });
+    localStorage.setItem('user', JSON.stringify(providerData[0]));
+    }else{
+      setIsMenu(!isMenu);
+    }
+
+
   }
 
   return (
@@ -41,8 +57,30 @@ const Header = () => {
         <div className="relative"> 
           <motion.img 
           whileTap={{scale: 0.6}} 
-          src={Avatar} className="w-10 min-w-[40px] drop-shadow-xl cursor-pointer" 
+          src={user ? user.photoURL : Avatar} 
+          className="w-10 min-w-[40px] drop-shadow-xl cursor-pointer rounded-full" 
           alt="userProfile" onClick={login} />
+          {
+            isMenu && (
+               <motion.div
+                initial={{opacity :0, scale: 0.6}} 
+                animate ={{opacity :1,scale: 1}}
+                exit = {{opacity :0,scale: 0.6}}
+               className=' w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12
+          2 right-0 '>
+            {
+              user && user.email === "hughesmbumba@gmail.com" && (
+              <Link to ={"/createItem"}>
+              <p className=" px4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd/>
+              </p>
+              </Link>
+              ) 
+            }
+            <p className=" px4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout < MdLogout /></p>
+
+          </motion.div>
+            )
+          }
         </div>
 
        
---
+++ src/context/initialState.js
@@ -0,0 +1,8 @@
+import { fetchUser } from "../Utils/fetchLocalStorageData"
+
+const userInfo = fetchUser()
+
+export const initialState = {
+    user: userInfo,
+}
+
